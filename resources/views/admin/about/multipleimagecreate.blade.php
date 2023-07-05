3c5f239e651b8f26182564c597055c4dbe0d575e
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-05 19:34:39 +0530

    multi image showing completed

--- resources/views/admin/about/multipleimagecreate.blade.php
+++ resources/views/admin/about/multipleimagecreate.blade.php
@@ -1,5 +1,7 @@
 @extends('admin.layout.app')
 @section('content')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <div class="page-content">
         <div class="container-fluid">
 
@@ -13,37 +15,65 @@
                             <form method="post" action="" enctype="multipart/form-data">
                                 @csrf
 
-
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label">About Multi Image
                                     </label>
                                     <div class="col-sm-10">
-                                        <input name="image[]" class="form-control" type="file" id="image"
-                                            multiple="">
+                                        <input name="image[]" class="form-control" type="file" id="image" multiple>
                                     </div>
                                 </div>
                                 <!-- end row -->
-
 
                                 <div class="row mb-3">
                                     <label for="example-text-input" class="col-sm-2 col-form-label"> </label>
-                                    <div class="col-sm-10">
-                                        <img class="rounded avatar-lg" src="{{ url('no_image.jpg') }}" alt="Card image cap">
+                                    <div class="col-sm-10" id="imagePreview">
+                                          <img id="showImage" class="rounded avatar-lg" src="{{  url('no_image.jpg') }}" alt="Card image cap">
+
                                     </div>
                                 </div>
                                 <!-- end row -->
+
                                 <input type="submit" class="btn btn-info waves-effect waves-light" value="Add Multi Image">
                             </form>
-
-
-
                         </div>
                     </div>
                 </div> <!-- end col -->
             </div>
-
-
-
         </div>
     </div>
+    <style>
+        #imagePreview {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            gap: 10px;
+        }
+
+        #imagePreview img {
+            width: 100px;
+            height: 100px;
+            object-fit: cover;
+        }
+    </style>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#image').change(function(e) {
+                var files = e.target.files;
+                var previewContainer = $('#imagePreview');
+                previewContainer.empty();
+
+                for (var i = 0; i < files.length; i++) {
+                    var reader = new FileReader();
+                    reader.onload = (function(file) {
+                        return function(e) {
+                            var image = $('<img>').attr('src', e.target.result);
+                            previewContainer.append(image);
+                        };
+                    })(files[i]);
+                    reader.readAsDataURL(files[i]);
+                }
+            });
+        });
+    </script>
 @endsection
