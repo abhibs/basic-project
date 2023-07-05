e52bf110725325d23564952ed9acb39711150059
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-05 11:01:11 +0530

    header and sidebar showing  admin image and name

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -74,14 +74,16 @@
                 </button>
             </div>
 
-
+            <?php
+            $admin= App\Models\Admin::first();
+            ?>
 
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="{{ asset('admin/assets/images/users/avatar-1.jpg') }}"
+                    <img class="rounded-circle header-profile-user" src="{{ !empty($admin->image) ? url('storage/admin/' . $admin->image) : url('no_image.jpg') }}"
                         alt="Header Avatar">
-                    <span class="d-none d-xl-inline-block ms-1">Julia</span>
+                    <span class="d-none d-xl-inline-block ms-1">{{ $admin->name }}</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,16 +1,19 @@
 <div class="vertical-menu">
 
     <div data-simplebar class="h-100">
-
+        <?php
+        $admin = App\Models\Admin::first();
+        ?>
         <!-- User details -->
         <div class="user-profile text-center mt-3">
             <div class="">
-                <img src="{{ asset('admin/assets/images/users/avatar-1.jpg') }}" alt="" class="avatar-md rounded-circle">
+                <img src="{{ !empty($admin->image) ? url('storage/admin/' . $admin->image) : url('no_image.jpg') }}"
+                    alt="" class="avatar-md rounded-circle">
             </div>
             <div class="mt-3">
-                <h4 class="font-size-16 mb-1">Julia Hudda</h4>
-                <span class="text-muted"><i
-                        class="ri-record-circle-line align-middle font-size-14 text-success"></i> Online</span>
+                <h4 class="font-size-16 mb-1">{{ $admin->name }}</h4>
+                {{-- <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i>
+                    Online</span> --}}
             </div>
         </div>
 
@@ -22,8 +25,7 @@
 
                 <li>
                     <a href="index.html" class="waves-effect">
-                        <i class="ri-dashboard-line"></i><span
-                            class="badge rounded-pill bg-success float-end">3</span>
+                        <i class="ri-dashboard-line"></i><span class="badge rounded-pill bg-success float-end">3</span>
                         <span>Dashboard</span>
                     </a>
                 </li>
