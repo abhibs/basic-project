8c0571e028fff488bfdc41e26aa1b44fce787ef9
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-08 16:37:18 +0530

    contact create page created

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QueryController;
+use App\Http\Controllers\ContactController;
+
 
 
 
 Route::get('', [HomeController::class, 'index'])->name('home');
 Route::post('queries', [QueryController::class, 'store'])->name('query-store');
+Route::get('contact', [ContactController::class, 'create'])->name('contact-create');
+
 
 
