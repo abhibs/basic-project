d1d7e85efa1fb9be11ccd31eef417c4fce067c42
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-08 21:40:22 +0530

    user site active link issue fix

--- resources/views/user/layout/header.blade.php
+++ resources/views/user/layout/header.blade.php
@@ -1,3 +1,7 @@
+@php
+    $route = Route::current()->getName();
+@endphp
+
 <header>
     <div id="sticky-header" class="menu__area transparent-header">
         <div class="container custom-container">
@@ -10,27 +14,29 @@
                                 <a href="{{ route('home') }}" class="logo__black"><img
                                         src="{{ asset('user/assets/img/logo/logo_black.png') }}" alt=""></a>
                                 <a href="{{ route('home') }}" class="logo__white"><img
-                                        src="{{ asset('user/assets/img/logo/logo_white.png') }}"
-                                        alt=""></a>
+                                        src="{{ asset('user/assets/img/logo/logo_white.png') }}" alt=""></a>
                             </div>
                             <div class="navbar__wrap main__menu d-none d-xl-flex">
                                 <ul class="navigation">
-                                    <li class="active"><a href="{{ route('home') }}">Home</a></li>
-                                    <li><a href="{{ route('home-about') }}">About</a></li>
+                                    <li class="{{ $route == 'home' ? 'active' : '' }}"><a
+                                            href="{{ route('home') }}">Home</a></li>
+                                    <li class="{{ $route == 'home-about' ? 'active' : '' }}"><a
+                                            href="{{ route('home-about') }}">About</a></li>
                                     {{-- <li><a href="services-details.html">Services</a></li> --}}
-                                    <li class="menu-item-has-children"><a href="{{ route('home-portfolio') }}">Portfolio</a>
+                                    <li class="{{ $route == 'home-portfolio' ? 'active' : '' }}"><a
+                                            href="{{ route('home-portfolio') }}">Portfolio</a>
                                         {{-- <ul class="sub-menu">
                                             <li><a href="portfolio.html">Portfolio</a></li>
                                             <li><a href="portfolio-details.html">Portfolio Details</a></li>
                                         </ul> --}}
                                     </li>
-                                    <li class="menu-item-has-children"><a href="{{ route('home-blog') }}">Our Blog</a>
+                                    <li class="{{ $route == 'home-blog' ? 'active' : '' }}"><a href="{{ route('home-blog') }}">Our Blog</a>
                                         {{-- <ul class="sub-menu">
                                             <li><a href="blog.html">Our News</a></li>
                                             <li><a href="blog-details.html">News Details</a></li>
                                         </ul> --}}
                                     </li>
-                                    <li><a href="{{ route('contact-create') }}">contact me</a></li>
+                                    <li class="{{ $route == 'contact-create' ? 'active' : '' }}"><a href="{{ route('contact-create') }}">contact me</a></li>
                                 </ul>
                             </div>
                             {{-- <div class="header__btn d-none d-md-block">
@@ -44,11 +50,9 @@
                             <div class="close__btn"><i class="fal fa-times"></i></div>
                             <div class="nav-logo">
                                 <a href="index.html" class="logo__black"><img
-                                        src="{{ asset('user/assets/img/logo/logo_black.png') }}"
-                                        alt=""></a>
+                                        src="{{ asset('user/assets/img/logo/logo_black.png') }}" alt=""></a>
                                 <a href="index.html" class="logo__white"><img
-                                        src="{{ asset('user/assets/img/logo/logo_white.png') }}"
-                                        alt=""></a>
+                                        src="{{ asset('user/assets/img/logo/logo_white.png') }}" alt=""></a>
                             </div>
                             <div class="menu__outer">
                                 <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
