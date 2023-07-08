a7f82039241edf47c08ded3fcf6f0b489197fe8a
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-08 17:59:45 +0530

    user portfolio page show completed

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Portfolio;
 
 class PortfolioController extends Controller
 {
-    //
+    public function portfolio(){
+
+        $datas = Portfolio::latest()->get();
+        return view('user.portfolio',compact('datas'));
+       }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QueryController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PortfolioController;
+
 
 
 
@@ -15,6 +17,8 @@ Route::post('queries', [QueryController::class, 'store'])->name('query-store');
 Route::get('contact', [ContactController::class, 'create'])->name('contact-create');
 Route::post('contact/store', [ContactController::class, 'store'])->name('contact-store');
 Route::get('about', [AboutController::class, 'about'])->name('home-about');
+Route::get('portfolio', [PortfolioController::class, 'portfolio'])->name('home-portfolio');
+
 
 
 
