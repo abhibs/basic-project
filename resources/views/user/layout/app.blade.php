9e47f984bcd0d3417ed4c65bee406944b60fcdca
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-08 15:09:46 +0530

    home query toastr message completed

--- resources/views/user/layout/app.blade.php
+++ resources/views/user/layout/app.blade.php
@@ -20,6 +20,11 @@
     <link rel="stylesheet" href="{{ asset('user/assets/css/default.css') }}">
     <link rel="stylesheet" href="{{ asset('user/assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('user/assets/css/responsive.css') }}">
+    <!-- toastr Css start-->
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+
+    <!-- toastr Css End-->
 </head>
 
 <body>
@@ -69,6 +74,27 @@
     <script src="{{ asset('user/assets/js/wow.min.js') }}"></script>
     <script src="{{ asset('user/assets/js/plugins.js') }}"></script>
     <script src="{{ asset('user/assets/js/main.js') }}"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        @if (Session::has('message'))
+            var type = "{{ Session::get('alert-type', 'info') }}"
+            switch (type) {
+                case 'info':
+                    toastr.info(" {{ Session::get('message') }} ");
+                    break;
+                case 'success':
+                    toastr.success(" {{ Session::get('message') }} ");
+                    break;
+                case 'warning':
+                    toastr.warning(" {{ Session::get('message') }} ");
+                    break;
+                case 'error':
+                    toastr.error(" {{ Session::get('message') }} ");
+                    break;
+            }
+        @endif
+    </script>
+    <!-- toastr js end -->
 </body>
 
 </html>
