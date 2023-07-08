29dbf196b2a65670675d05fb9008ce61a128424f
abhibs <abhiram.bs@webdigitalmantra.in>
2023-07-08 17:30:43 +0530

    admin contact sidebar completed

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -101,6 +101,17 @@
                     </ul>
                 </li>
 
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="ri-profile-line"></i>
+                        <span>User Contact List</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="{{ route('user-contact') }}">All User Contact List</a></li>
+
+                    </ul>
+                </li>
+
 
 
             </ul>
